c59bf2a8e4517366d0137ee3f6e11f33fe9d59a6
derekgilwa <dgliwa@thoughtworks.com>
2016-05-09 14:23:44 -0500

    initial conversion from fahrenheit to celsius

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,15 +13,37 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    @IBAction func fahrenheitFieldChanged(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusValue()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
         } else {
-            celsiusLabel.text = "???"
+            return nil
+        }
+    }
+    
+    @IBAction func fahrenheitFieldChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
         textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusValue() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
